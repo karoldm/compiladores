bae3215dd61c3ea8736d97293ab139d889b716fe
karol marques <karoldm1579@gmail.com>
2023-10-01 12:33:54 -0300

    fix: last word expected

--- src/functions/parser.ts
+++ src/functions/parser.ts
@@ -16,20 +16,38 @@ export class Parser {
       return this.errors;
   }
 
-  consumeToken(expectedTokenType: any, sinc?: any) {
+  createPattern(expectedPattern: string) {
+    return `^${expectedPattern}$`;
+  }
+
+  consumeToken(expectedToken: string, sinc?: string) {
       const currentToken = this.tokens[this.currentTokenIndex];
-      if (currentToken && expectedTokenType.test(`/^${currentToken.token}^/`)) {
+
+      if(this.currentTokenIndex >= this.tokens.length){
+        this.errors.push(`Esperado ${expectedToken}`);
+        this.currentTokenIndex++;
+        return;
+      }
+        
+      const regexToken = new RegExp(this.createPattern(expectedToken), 'g');
+
+      console.log(expectedToken)
+
+      if (currentToken && regexToken.test(currentToken.token)) {
           this.currentTokenIndex++;
       } else {
-          this.errors.push(`Esperado ${expectedTokenType}, encontrado ${currentToken.token}
+          this.errors.push(`Esperado ${expectedToken}, encontrado ${currentToken.token}
           na linha ${currentToken.linha}, coluna inicial ${currentToken.coluna_inicial} até 
           coluna final ${currentToken.coluna_final}`);
 
           if(sinc) {
+            const regexSinc = new RegExp(expectedToken);
+
             while(this.currentTokenIndex < this.tokens.length && 
-                !sinc.test(`/^${this.tokens[this.currentTokenIndex].token}$/`) ){
+                !regexSinc.test(this.tokens[this.currentTokenIndex].token) ){
                 this.currentTokenIndex++;
             }
+            this.currentTokenIndex++;
         }
       }
   }
@@ -37,10 +55,10 @@ export class Parser {
   programa() {
     this.consumeToken('PROGRAM', 'IDENTIFICADOR');
     this.consumeToken('IDENTIFICADOR', 'PONTVIRG');
-    this.consumeToken('PONTOVIRG', 'VAR|INT|BOOLEAN|PROCEDURE|BEGIN|IF|WHILE|READ|WRITE');
+    this.consumeToken('PONTOVIRG', '(VAR|INT|BOOLEAN|PROCEDURE|BEGIN|IF|WHILE|READ|WRITE)');
     
     this.bloco();
-    this.consumeToken('PONTO');
+    this.consumeToken('PONTO', '');
   }
 
   bloco() {
@@ -51,9 +69,9 @@ export class Parser {
 
   declaracaoVariaveis() {
       this.declaracaoVariavel();
-      while (this.tokens[this.currentTokenIndex].token === 'PONTOVIRG' && 
-      this.currentTokenIndex < this.tokens.length) {
-          this.consumeToken('PONTOVIRG', 'INT|BOOLEAN');
+      while (this.currentTokenIndex < this.tokens.length && 
+            this.tokens[this.currentTokenIndex].token === 'PONTOVIRG') {
+          this.consumeToken('PONTOVIRG', '(INT|BOOLEAN)');
           this.declaracaoVariavel();
       }
   }
@@ -64,14 +82,15 @@ export class Parser {
   }
 
   tipo() {
-    this.consumeToken('INT|BOOLEAN', 'IDENTIFICADOR');
+    this.consumeToken('(INT|BOOLEAN)', 'IDENTIFICADOR');
   }
 
   listaIdentificadores() {
-      this.consumeToken('IDENTIFICADOR', 'INT|BOOLEAN|PONTOVIRG');
-      while (this.tokens[this.currentTokenIndex].token === 'VIRG') {
+      this.consumeToken('IDENTIFICADOR', '(INT|BOOLEAN|PONTOVIRG)');
+      while (this.currentTokenIndex < this.tokens.length && 
+            this.tokens[this.currentTokenIndex].token === 'VIRG') {
           this.consumeToken('VIRG', 'IDENTIFICADOR');
-          this.consumeToken('IDENTIFICADOR', 'VIRG|PONTOVIRG');
+          this.consumeToken('IDENTIFICADOR', '(VIRG|PONTOVIRG)');
       }
   }
 }
